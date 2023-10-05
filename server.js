91427b258b61f4f8f5be5aa31563923b67403dd1
Dayton Kotas <Kotasdm@gmatc.matc.edu>
2023-10-04 22:27:21 -0500

    Updated Project

--- server.js
+++ server.js
@@ -7,8 +7,42 @@ const app = express();
 // Connect database
 connectDatabase();
 
-app.get("/", (req, res) =>
-  res.send("http get request sent to root api endpoint")
+// Configure Middleware
+app.use(express.json({ extended: false }));
+
+// API endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint 
+ */
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
+);
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name')
+            .not()
+            .isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6 })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            return res.send(req.body);
+        }
+    }
 );
 
 // Connection Listener
